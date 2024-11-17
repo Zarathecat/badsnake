4208f22831903b3bed1d24b20355a072728aba97
Zara <thezarazia@gmail.com>
2024-11-17 02:09:14 +0000

    Remove old destroy_food() method from food.gd
    We no longer use it. We also comment out some debugging code in
    the ready() function, since we haven't needed it for a while.

--- food.gd
+++ food.gd
@@ -2,11 +2,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("food ready")
+	pass #print("food ready")#debug
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-func destroy_food():
-	queue_free()
