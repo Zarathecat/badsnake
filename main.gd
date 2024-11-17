a581fc0084e379d94ad8cf78d4c0a632680380fa
Zara <thezarazia@gmail.com>
2024-11-17 01:45:40 +0000

    Remove immortal_flag and LunchTimer
    The fixes to collision detection make these redundant;
    remove.

--- main.gd
+++ main.gd
@@ -8,11 +8,10 @@ var window_size = DisplayServer.window_get_size()
 # used to trigger the snake's growth, score update etc
 var eat_flag = false
 
-# immortal_flag is set when the snake has *just* eaten.
-# This is used as a workaround to disable collision-detection
-# for the snake with its new segment when it first gets it,
-# so it doesn't instantly die by growing
-var immortal_flag = false
+# immortal_flag was used to disable collision-detection.
+# No longer needed, but kept around as a comment in case
+# we reintroduce it as a powerup sometime
+#var immortal_flag = false
 
 # Self-explanatory. Says it's time for the snake to die.
 var death_flag = false
@@ -22,8 +21,8 @@ var death_flag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	immortal_flag = true
-	$LunchTimer.start() # Hack to avoid starting the game with a gameover
+	# Avoids starting with a game over.
+	$Segment/CollisionShape2D.disabled = true
 	$Segment.add_to_group("segments")
 	food_scene.instantiate()
 	
@@ -33,7 +32,6 @@ func _process(delta: float) -> void:
 	# If snake has just eaten some food	
 	if eat_flag == true:
 		print("ate food!")
-		$LunchTimer.start()
 		score += 1
 		print("Score: " + str(score))
 		make_food()
@@ -63,11 +61,11 @@ func _process(delta: float) -> void:
 		if segments.size() > 1:
 			segments[-2].get_node("CollisionShape2D").disabled = false
 		for i in range(0, snake_length-1):
-			# Move each segment along by 1
+			# Move each segment along by 1 place
 			segments[i].position = segments[i+1].position
 		# When the head updates too slowly, the first segment crashes
-		# into it. So collision detection for the first segment should
-		# always be disabled.
+		# into it. We disable the collision-detection elsewhere.
+		# Move front of snake (end of array) forwards
 		segments[-1].position = $Head.position
 	check_snake($Head.position[0], $Head.position[1])
 	if death_flag == true:
@@ -84,13 +82,14 @@ func check_snake(a, b):
 # Add a segment to the snake, and thus to the scene tree. Track it in the
 # list of segment nodes. The position here is its starting position; after
 # this one-off it will be set by the game loop. We instantiate the segment
-# offscreen and then have it join the snake on the first loop, so we don't
-# hit it.
+# offscreen and then have it join the snake on the first loop; smoother.
 func grow_snake():
 	var segment = segment_scene.instantiate()
 	var segments = get_tree().get_nodes_in_group("segments")
-	segment.position = Vector2(window_size.x + 50, window_size.y +50) # hack
-	# The new node can't detect collisions, so that it doesn't hit the head
+	# 'winsize+50' is arbitrary choice of offscreen location.
+	segment.position = Vector2(window_size.x + 50, window_size.y +50)
+	# We turn off node's collision-detection,
+	# so that it doesn't hit the head
 	# when the head is slow to update its own position.
 	segment.get_node("CollisionShape2D").disabled = true
 	add_child(segment)
@@ -137,21 +136,15 @@ func game_over():
 	score = 0
 	death_flag = false # auto-restart
 
-# immortal_flag is used here as a hack for turning off
-# collision-detection briefly, so the snake won't die if it's lunching.
 func _on_head_area_entered(area: Area2D) -> void:
-	if immortal_flag == false:
-		death_flag = true
-		#print("I hit myself!") # debugging
+	death_flag = true
+	#print("I hit myself!") # debugging
 
 # We disable collision-detection so that the snake only eats one
 # item of food, and the score doesn't skyrocket. 
-# We never bother to enable it again, because we then destroy the
-# item of food.
 func _on_food_area_entered(area: Area2D) -> void:
 	$Food/CollisionShape2D.set_deferred(&"disabled", true)
 	eat_flag = true
-	immortal_flag = true
 	
-func _on_lunch_timer_timeout() -> void:
-	immortal_flag = false
+#func _on_lunch_timer_timeout() -> void:
+#	immortal_flag = false
